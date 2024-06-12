bd0fe41ba8f4f195998ac274ee46ce1ae9f21464
Martin Armbruster <armbrustermartin@googlemail.com>
2024-06-13 00:17:03 +0200

    Switched to a JSON-based result file, and added an optional condition for counting occurrences.

--- Survey2024_PerformanceManagement/code/src/main.ts
+++ Survey2024_PerformanceManagement/code/src/main.ts
@@ -1,5 +1,4 @@
 import { XMLParser } from 'fast-xml-parser';
-import { parse } from 'csv-parse/sync';
 import { existsSync } from 'fs';
 import { readFile, writeFile } from 'fs/promises';
 import { resolve } from 'path';
@@ -7,10 +6,13 @@ import { Question, QuestionnaireXML, ResponseCategory } from './questionnaire.js
 import { QUESTION_CODES } from './question-codes.js';
 import * as Plot from '@observablehq/plot';
 import { JSDOM } from 'jsdom';
+import { ResponseJson } from './responses.js';
 
 const Other: string = 'Other';
 const OtherCode: string = `[${Other}]`;
 const NoAnswer: string = 'No Answer';
+const Yes: string = 'Yes';
+const No: string = 'No';
 
 function readFileContent(path: string): Promise<string> {
     if (!existsSync(path)) {
@@ -25,8 +27,8 @@ async function parseQuestionnaire(): Promise<Question[]> {
     return survey.questionnaire.section.flatMap((section) => section.question);
 }
 
-async function parseAnswers(): Promise<string[][]> {
-    return parse(await readFileContent(resolve('..', 'results.csv')), { bom: true });
+async function parseAnswers(): Promise<ResponseJson> {
+    return JSON.parse(await readFileContent(resolve('..', 'results.json')));
 }
 
 function extractReponseValues(responses: ResponseCategory | ResponseCategory[]): string[] {
@@ -52,13 +54,25 @@ type Tuple = {
 
 type Triple = Tuple & { z: string; };
 
-function countResponseOccurrences(answers: string[][], code: string, responseValues: string[]): Tuple[] {
+type Condition = {
+    code: string;
+    value: string;
+};
+
+type CodeConditionPair = {
+    code: string;
+    condition?: Condition;
+};
+
+function countResponseOccurrences(answers: ResponseJson, { code, condition }: CodeConditionPair, responseValues: string[]): Tuple[] {
     const result: Tuple[] = responseValues.map((value) => ({ x: value, y: 0 }));
-    const columnIdx: number = answers[0].indexOf(code);
     let noAnswerCount: number = 0;
-    for (let rowIdx: number = 1; rowIdx < answers.length; rowIdx++) {
-        if (answers[rowIdx][columnIdx] !== '') {
-            result.filter((x) => x.x === answers[rowIdx][columnIdx]).forEach((x) => x.y++);
+    for (const entry of answers.responses) {
+        if (condition && entry[condition.code] !== condition.value) {
+            continue;
+        }
+        if (entry[code] !== '') {
+            result.filter((x) => x.x === entry[code]).forEach((x) => x.y++);
         } else {
             noAnswerCount++;
         }
@@ -114,13 +128,40 @@ async function main(): Promise<void> {
     // questionCodesSourceCode += '};\n';
     // await writeFile(resolve('src', 'question-codes.ts'), questionCodesSourceCode, { encoding: 'utf-8' });
     
-    const answers: string[][] = await parseAnswers(); // [row][column]
+    const answers: ResponseJson = await parseAnswers();
 
     const virtualDom = new JSDOM();
 
-    const codesForDescriptiveStatistics: string[] = [QUESTION_CODES.D2Age, QUESTION_CODES.D3Experience, QUESTION_CODES.D5CompanySize, QUESTION_CODES.D6TeamSize];
-    for (const code of codesForDescriptiveStatistics) {
-        const responseCount: Tuple[] = countResponseOccurrences(answers, code, codeToResponses.get(code) ?? []);
+    const codesForDescriptiveStatistics: CodeConditionPair[] = [
+        { code: QUESTION_CODES.D2Age },
+        { code: QUESTION_CODES.D3Experience },
+        { code: QUESTION_CODES.D5CompanySize },
+        { code: QUESTION_CODES.D6TeamSize },
+        { code: QUESTION_CODES.C8RelevanceSQ001 },
+        { code: QUESTION_CODES.C8RelevanceSQ002 },
+        { code: QUESTION_CODES.C9GeneralTrustSQ001 },
+        { code: QUESTION_CODES.C9GeneralTrustSQ002 },
+        { code: QUESTION_CODES.C9GeneralTrustSQ003 },
+        { code: QUESTION_CODES.Ch1QualitySQ001 },
+        { code: QUESTION_CODES.Ch1QualitySQ002 },
+        { code: QUESTION_CODES.Ch1QualitySQ003 },
+        { code: QUESTION_CODES.Ch1QualitySQ004 },
+        { code: QUESTION_CODES.Ch1QualitySQ005 },
+        { code: QUESTION_CODES.Ch4PredictionFeatureSQ001 },
+        { code: QUESTION_CODES.Ch4PredictionFeatureSQ002 },
+        { code: QUESTION_CODES.Ch4PredictionFeatureSQ003 },
+        { code: QUESTION_CODES.Ch4PredictionFeatureSQ004 },
+        { code: QUESTION_CODES.Ch4PredictionFeatureSQ005 },
+        { code: QUESTION_CODES.N2TrustSQ001 },
+        { code: QUESTION_CODES.N2TrustSQ002 },
+        { code: QUESTION_CODES.N2TrustSQ003 },
+        { code: QUESTION_CODES.Co2DevTimeLearn, condition: { code: QUESTION_CODES.D4RolesSQ003, value: No } },
+        { code: QUESTION_CODES.Co2PMTimeLearn, condition: { code: QUESTION_CODES.D4RolesSQ003, value: Yes } },
+        { code: QUESTION_CODES.Co3DevTimeAdoption, condition: { code: QUESTION_CODES.D4RolesSQ003, value: No } },
+        { code: QUESTION_CODES.Co3PMTimeAdoption, condition: { code: QUESTION_CODES.D4RolesSQ003, value: Yes } }
+    ];
+    for (const coco of codesForDescriptiveStatistics) {
+        const responseCount: Tuple[] = countResponseOccurrences(answers, coco, codeToResponses.get(coco.code) ?? []);
         const svgElement = Plot.plot({
             grid: true,
             x: { label: '' },
@@ -132,7 +173,7 @@ async function main(): Promise<void> {
             document: virtualDom.window.document
         });
         const svg: string = svgElement instanceof virtualDom.window.SVGElement ? svgElement.outerHTML : svgElement.innerHTML;
-        await writeFile(`${unformatCode(code)}.svg`, svg, { encoding: 'utf-8' });
+        await writeFile(`${unformatCode(coco.code)}${coco.condition ? '-' + coco.condition.code + '-' + coco.condition.value : ''}.svg`, svg, { encoding: 'utf-8' });
     }
 }
 
---
+++ Survey2024_PerformanceManagement/code/src/responses.ts
@@ -0,0 +1,5 @@
+export type ResponseEntry = { [key: string]: string | number | null };
+
+export type ResponseJson = {
+    responses: ResponseEntry[];
+};
